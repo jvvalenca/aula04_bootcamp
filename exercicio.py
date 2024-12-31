458da051a18c368ec92736a049a575bfef32d424
joão vitor <joaovitorvalenca92@gmail.com>
2024-12-31 17:25:18 -0300

    adicionando mais arquivos

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+lista_de_livros_usando_dict: dict = {
+    "livro_01": {"Título": "Game of Thrones",
+    "Autor": "João",
+    "Ano": 2005 },
+    "livro_02": {"Título": "Game of Thrones 2",
+    "Autor": "João",
+    "Ano": 2007 }
+    
+}
